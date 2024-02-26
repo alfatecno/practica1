e2dd6ac06744a2a417e06c7d7ae3766444a75167
Alberto Gonzalez <alberto020599@hotmail.com>
2024-02-26 16:25:41 -0600

    fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,18 +9,33 @@ export class InfoPaginaService {
 
   info: InfoPagina ={};
   cargada = false;
+  equipo: any;
 
   constructor( private http: HttpClient) {
 
    // console.log('servicio de infopagina ,listo');
     //leer el aricho json y tomar sus propiedades
+   this.cargarInfo();
+   this.cargarEquipo();
+  }
+
+  private cargarInfo(){
 
     this.http.get('assets/data/data-pagina.json')
                .subscribe( (resp: InfoPagina) =>{
                 this.cargada = true;
                 this.info = resp;
-                console.log(resp);
 
-               })
+
+               });
+  }
+  private cargarEquipo(){
+
+    this.http.get('https://proyecto-1-3cf3e-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe( (resp:any ) =>{
+     this.equipo = resp; //la respuesta generada
+     console.log(resp);  //para mirarlo en la consola
+
+    });
   }
 }
