d7049ee327f1642f63954445a6fc5245fb56ae1e
Alberto Gonzalez <alberto020599@hotmail.com>
2024-02-27 12:42:00 -0600

    fin seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -11,6 +11,7 @@ export class ProductosService {
 
  cargando = true;
  productos: Producto[] = [];
+ productosFiltrado: Producto[] = [];
 
 
 
@@ -22,18 +23,50 @@ export class ProductosService {
 
 
   private cargarProductos() {
-
-    this.http.get<Producto[]>('https://proyecto-1-3cf3e-default-rtdb.firebaseio.com/productos_idx.json')
-    .subscribe((resp: Producto[]) => {
-
-       this.productos = resp;
-       this.cargando = false;
+    return new Promise<void>((resolve, reject) => {
+        this.http.get<Producto[]>('https://proyecto-1-3cf3e-default-rtdb.firebaseio.com/productos_idx.json')
+            .subscribe((resp: Producto[]) => {
+                this.productos = resp;
+                this.cargando = false;
+                resolve(); // Aquí no se pasa ningún argumento a resolve porque no se necesitan datos adicionales.
+            });
     });
-  }
+}
 
   getProducto (id: string){
 
     return this.http.get(`https://proyecto-1-3cf3e-default-rtdb.firebaseio.com/productos/${id}.json`) //agregar backtick para insercciones de pedazos
   }
 
+
+  buscarProducto (termino : string){
+
+    if (this.productos.length===0){
+      this.cargarProductos().then(() =>{
+        //despues de tener los productos
+        this.filtrarProductos(termino);//aplicar filtro
+      }) ;  //cargar productos
+    }else{
+      this.filtrarProductos(termino);   //aplicar filtro
+    }
+
+  }
+
+  private filtrarProductos(termino: string){
+
+   this.productosFiltrado = [];
+
+   termino =termino.toLocaleLowerCase();
+
+   this.productos.forEach( prod =>{
+
+    const tituloLower = prod.titulo.toLocaleLowerCase();
+
+   if( prod.categoria.indexOf(termino) >=0 || tituloLower.indexOf( termino )>=0){
+
+     this.productosFiltrado.push (prod);
+   }
+   })
+  }
 }
+//`
